0a2d3dc16c41b4e3eff36491da9b1e7d84177e0f
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-13 16:29:20 -0500

    [Add] Added dummy data todo array of objects.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ class App extends React.Component {
   constructor(){
     super();
     this.state={
-      
+      todo: [
+        {
+          task: 'Organize Garage',
+          id: 1528817077286,
+          completed: false
+        },
+        {
+          task: 'Bake Cookies',
+          id: 1528817084358,
+          completed: false
+        }
+      ]
     }
   }
   // you will need a place to store your state in this component.
