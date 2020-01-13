16687e56785f7c965dd95267367b625c3e506317
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-13 17:09:02 -0500

    [Add] Added Change Handler and Form Inputs/Buttons/Props to Form Component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 //Components
 import TodoList from './components/TodoComponents/TodoList';
+import TodoForm from './components/TodoComponents/TodoForm';
 
 class App extends React.Component {
   constructor(){
     super();
     this.state={
+      task: '',
       todo: [
         {
           task: 'Organize Garage',
@@ -21,14 +23,21 @@ class App extends React.Component {
       ]
     }
   }
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+
+  handleChanges = e => {
+    this.setState({task: e.target.value});
+    
+  }
+
   render() {
+   const {task, todo} = this.state;
+
+   console.log(task)
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-        <TodoList todos={this.state.todo} />
+        <TodoList todos={todo} />
+        <TodoForm task={task} handleChanges={this.handleChanges} />
       </div>
     );
   }
--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const TodoForm = () => {
+const TodoForm = ({task, handleChanges}) => {
     return(
         <div>
-            <form action=""></form>
+            <form action="">
+                <input type="text" name="task" id="task" value={task} onChange={handleChanges}/>
+                <button type="submit">Submit</button>
+                <button type="reset">Clear Completed</button>
+            </form>
         </div>
     )
 }
