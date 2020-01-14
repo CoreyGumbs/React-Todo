307f3dc5643694ed2355cee188a694018f90ae5c
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-14 13:15:47 -0500

    Styles Updated And Completed

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 const Todo= ({todo}) => { 
     return(
-        <div className="todo-task-container">
-            <p className={todo.completed ? 'completed': ' '}>Task: {todo.task}</p>
-            <p className={todo.completed ? 'completed': ' '}>Completed: {todo.completed ? "Yes" : "No"}</p>
-            <p className={todo.completed ? 'completed': ' '}>ID:{ todo.id }</p>
+        <div className={todo.completed ? "todo-task-container task-completed" :"todo-task-container"}>
+            <h3 className={todo.completed ? 'todo-task-content completed': 'todo-task-content'}>{todo.task}</h3>
+            <p className={'todo-task-content'}>
+                {todo.completed ? <span id="complete-yes">Completed</span>:<span id="complete-no"> Not Completed</span>}
+            </p>
+            <p className={'todo-task-content todo-id'}>ID:{ todo.id }</p>
         </div>
     )
 }
